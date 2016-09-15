449fac1c20ce1fd33937dc57da66cc041712829e
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-15 19:16:54 +0300

    1

---
+++ turtleSquare.py
@@ -0,0 +1,16 @@
+__author__ = 'student'
+import turtle
+import time
+
+turtle.shape('turtle')
+turtle.penup()
+turtle.goto(-100,-100)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+time.sleep(10)
