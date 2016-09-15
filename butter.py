44efd9029e114522ffaca9c50bdd1abc8bb43059
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-15 21:31:33 +0300

    1

---
+++ butter.py
@@ -0,0 +1,12 @@
+__author__ = 'student'
+__author__ = 'student'
+import turtle
+import math
+s=0.
+R=50.
+
+
+for i in range (100):
+    turtle.circle(R+5*i)
+    turtle.circle(-R-5*i)
+
---
+++ turtleflover.py
@@ -0,0 +1,11 @@
+__author__ = 'student'
+import turtle
+import math
+s=0.
+R=50.
+
+
+for i in range (100):
+    turtle.circle(R)
+    turtle.circle(-R)
+    turtle.right(40)
