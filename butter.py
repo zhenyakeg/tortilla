cf4deb9c9385c52d21cb9c66f03218fe85bc4174
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-15 21:40:25 +0300

    1

--- butter.py
+++ butter.py
@@ -2,11 +2,15 @@ __author__ = 'student'
 __author__ = 'student'
 import turtle
 import math
-s=0.
-R=50.
 
-
-for i in range (100):
-    turtle.circle(R+5*i)
-    turtle.circle(-R-5*i)
-
+R=50
+n=100
+def circ(r):
+    turtle.left(360/n)
+    turtle.forward(2*R*math.sin(math.pi/n))
+for i in range (30):
+    #turtle.circle(R+5*i)
+    #turtle.circle(-R-5*i)
+    circ(R+5*i)
+for i in range (300):
+    circ(-R-5*i)
