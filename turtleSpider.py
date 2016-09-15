172fff4b2aa4803b783115793788ba14a3a176ce
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-15 19:57:12 +0300

    1

--- turtleSpider.py
+++ turtleSpider.py
@@ -1,1 +1,11 @@
 __author__ = 'student'
+import turtle
+import time
+n=int(input())
+for i in range (0,n):
+    turtle.shape('turtle')
+    turtle.forward(150)
+    turtle.left(180)
+    turtle.forward(150)
+    turtle.right(180+360/n)
+time.sleep(10)
