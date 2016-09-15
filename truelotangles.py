bcc275d490ca0ba3e9cc53a2c33a4c39882caaa1
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-15 20:37:10 +0300

    1

---
+++ truelotangles.py
@@ -0,0 +1,6 @@
+__author__ = 'student'
+import turtle
+import math
+def angles(R,n):
+    a=math.sin(2*math.pi/2/n)*2*R
+    turtle.right
--- turtleSpiral.py
+++ turtleSpiral.py
@@ -6,6 +6,6 @@ dr = 5
 n = 10
 N = 20
 
-for i in range (r1,r1+n*dr,dr):
-    turtle.left()
-    turtle.forward(i)
+for i in range (10,100000000,1):
+    turtle.left(5)
+    turtle.forward(i/200)
